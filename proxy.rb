0247b0a57614eba289c54a8fe8a82d24078075e9
Dick Tang <dick.tang@ymail.com>
2015-12-01 11:05:13 +0800

    accept config from http header, not require AWS session-token

--- proxy.rb
+++ proxy.rb
@@ -6,16 +6,10 @@ require 'faraday_middleware/aws_signers_v4'
 require 'net/http/persistent'
 require 'yaml'
 
-config = YAML.load_file(File.dirname(File.expand_path(__FILE__)) + '/config.yaml')
+LISTEN_PORT = ARGV[0] || 8080
 
-UPSTREAM_URL = config['upstream_url']
-UPSTREAM_SERVICE_NAME = config['upstream_service_name']
-UPSTREAM_REGION = config['upstream_region']
-LISTEN_PORT = config['listen_port'] || 8080
-
-
-unless ENV['AWS_ACCESS_KEY_ID'].nil? || ENV['AWS_SECRET_ACCESS_KEY'].nil? || ENV['AWS_SESSION_TOKEN'].nil?
-  CREDENTIALS = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'],ENV['AWS_SECRET_ACCESS_KEY'],ENV['AWS_SESSION_TOKEN'])
+unless ENV['AWS_ACCESS_KEY_ID'].nil? || ENV['AWS_SECRET_ACCESS_KEY'].nil?
+  CREDENTIALS = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'],ENV['AWS_SECRET_ACCESS_KEY'])
 else
   CREDENTIALS = Aws::InstanceProfileCredentials.new
 end
@@ -23,16 +17,21 @@ end
 app = Proc.new do |env|
   postdata = env['rack.input'].read
 
-  client = Faraday.new(url: UPSTREAM_URL) do |faraday|
-    faraday.request(:aws_signers_v4, credentials: CREDENTIALS, service_name: UPSTREAM_SERVICE_NAME, region: UPSTREAM_REGION)
-    faraday.adapter(:net_http_persistent)
-  end
-
   headers = env.select {|k,v| k.start_with? 'HTTP_', 'CONTENT_' }
                 .map{|key,val| [ key.sub(/^HTTP_/,''), val ] }
                 .map{|key,val| { key.sub(/_/,'-') => val} }
                 .select {|key,_| key != 'HOST'}
                 .reduce Hash.new, :merge
+
+  upstream_protocol = headers["X-UPSTREAM-PROTOCOL"] || "https"
+  upstream_service_name = headers["X-UPSTREAM-SERVICE-NAME"] || "es"
+  upstream_region = headers["X-UPSTREAM-REGION"] || "us-east-1"
+  upstream_url = headers["X-UPSTREAM-URL"] || "#{upstream_protocol}://#{env['SERVER_NAME']}"
+
+  client = Faraday.new(url: upstream_url) do |faraday|
+    faraday.request(:aws_signers_v4, credentials: CREDENTIALS, service_name: upstream_service_name, region: upstream_region)
+    faraday.adapter(:net_http_persistent)
+  end
 
   if env['REQUEST_METHOD'] == 'GET'
     response = client.get "#{env['REQUEST_PATH']}?#{env['QUERY_STRING']}", {}, headers
